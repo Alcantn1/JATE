e0fc953ae94b464700fa7cb9a4a84926f80171c6
Alcantn1 <nalcantara92@gmail.com>
2023-06-12 20:52:35 -0700

    added code

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # JATE
+
+# Description
+
+
+
+## Installation
+
+N/A
+
+## Usage
+
+
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to the LICENSE in the repo.
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +15,71 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+      new WebpackPwaManifest({
+        icons: [
+          {
+            src: '/assets/icons/icon_512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/assets/icons/icon_384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+          },
+          {
+            src: '/assets/icons/icon_256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: '/assets/icons/icon_192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/assets/icons/icon_128x128.png',
+            sizes: '128x128',
+            type: 'image/png',
+          },
+          {
+            src: '/assets/icons/icon_96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+          },
+        ],
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        orientation: 'portrait',
+        display: 'standalone',
+        start_url: '/',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+      }),
+      new InjectManifest({
+        swSrc: './src/service-worker.js',
+      }),
     ],
-
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
